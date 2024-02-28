74855d2a1faeee9693b77e65b77732a8fe8fcfeb
thomascswalker <thomascswalker@gmail.com>
2024-02-28 14:39:07 -0500

    add linear color

--- Source/Private/Math/Color.cpp
+++ Source/Private/Math/Color.cpp
@@ -1,1 +1,40 @@
 ﻿#include "Math/Color.h"
+
+/* Srgb color */
+
+PColor PColor::FromRgba(const uint8 R, const uint8 G, const uint8 B, const uint8 A)
+{
+    PColor Color;
+    Color.R = R;
+    Color.G = G;
+    Color.B = B;
+    Color.A = A;
+    return Color;
+}
+void PColor::Clamp()
+{
+    for (uint8& V : RGBA)
+    {
+        V = V > 254 ? 255 : V;
+    }
+}
+
+/* Linear color */
+
+PLinearColor PLinearColor::FromRgba(float R, float G, float B, float A)
+{
+    PLinearColor Color;
+    Color.R = R;
+    Color.G = G;
+    Color.B = B;
+    Color.A = A;
+    return Color;
+}
+
+void PLinearColor::Clamp()
+{
+    for (float& V : RGBA)
+    {
+        V = V > 1.0f ? 1.0f : V;
+    }
+}
--- Source/Public/Math/Color.h
+++ Source/Public/Math/Color.h
@@ -2,6 +2,9 @@
 
 #include "Types.h"
 
+struct PColor;
+struct PLinearColor;
+
 struct PColor
 {
     union
@@ -16,13 +19,24 @@ struct PColor
         uint8 RGBA[4];
     };
 
-    static PColor FromRgba(const uint8 R, const uint8 G, const uint8 B, const uint8 A = 255)
+    static PColor FromRgba(const uint8 R, const uint8 G, const uint8 B, const uint8 A = 255);
+    void Clamp();
+};
+
+struct PLinearColor
+{
+    union
     {
-        PColor Color;
-        Color.R = R;
-        Color.G = G;
-        Color.B = B;
-        Color.A = A;
-        return Color;
-    }
+        struct // NOLINT
+        {
+            float R;
+            float G;
+            float B;
+            float A;
+        };
+        float RGBA[4];
+    };
+
+    static PLinearColor FromRgba(float R, float G, float B, float A = 1.0f);
+    void Clamp();
 };
